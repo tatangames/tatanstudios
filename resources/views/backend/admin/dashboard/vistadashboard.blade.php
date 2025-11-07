d4806039843a762b66a7aa2a43ec192711305338
Tatan <tatangamess@gmail.com>
2025-11-07 15:14:25 -0600

    cambios vista16

--- resources/views/backend/admin/dashboard/vistadashboard.blade.php
+++ resources/views/backend/admin/dashboard/vistadashboard.blade.php
@@ -3,6 +3,8 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/ultra.jpg') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/ultra.jpg') }}">
     <h1>Dashboard</h1>
 @stop
 {{-- Activa plugins que necesitas --}}
