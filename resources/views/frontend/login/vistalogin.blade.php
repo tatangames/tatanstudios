2293cae5544a66825bfc974bc52dbdb3796b0318
Tatan <tatangamess@gmail.com>
2025-11-07 15:08:13 -0600

    cambios vista15

--- resources/views/frontend/login/vistalogin.blade.php
+++ resources/views/frontend/login/vistalogin.blade.php
@@ -2,7 +2,7 @@
 <html lang="es">
 
 <head>
-    <title>Zona Fitness Gym</title>
+    <title>ULTRA FITNNES</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/login/bootstrap.min.css') }}">
